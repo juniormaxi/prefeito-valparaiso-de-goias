465f6ef728176f109713f7c7a551c30f3af8c72e
juniormaxi <juniormaxi@hotmail.com>
2024-05-09 18:50:04 -0300

    Add files via upload

---
+++ submit_form.php
@@ -0,0 +1,32 @@
+<?php
+// Verifica se os dados foram postados
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Coleta os dados do formulário
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+
+    // Prepara o conteúdo do e-mail
+    $to = 'juniormaxi@hotmail.com'; // Destinatário do email
+    $subject = 'Novo Contato do Site'; // Assunto do email
+    $message = "Você recebeu uma nova mensagem do site 'Em Quem Votar para Prefeito em Valparaíso de Goiás'.\n\n";
+    $message .= "Nome: $name\n";
+    $message .= "Email: $email\n";
+    $message .= "Telefone: $phone\n";
+
+    // Prepara os cabeçalhos do e-mail
+    $headers = "From: webmaster@example.com\r\n"; // Este email deve ser substituído pelo seu email de origem
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    // Envia o e-mail
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Falha ao enviar a mensagem.";
+    }
+} else {
+    // Não é POST
+    echo "Método de requisição inválido.";
+}
+?>
